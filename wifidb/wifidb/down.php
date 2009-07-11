114306b79ef11ee2a23f8579fe57e395e146514c
Phillip Ferland <pferland@randomintervals.com>
2009-07-11 03:49:47 +0000

    0.16 Build 3 release
    git-svn-id: https://svn.code.sf.net/p/vistumbler/code@414 ecfccc70-5fee-4fae-9362-317d472445a1

--- wifidb/wifidb/down.php
+++ wifidb/wifidb/down.php
@@ -20,6 +20,14 @@ include('lib/config.inc.php');
 <br>
 <table width="100%" border="2" id="16pb1">
 	<tr><td style="border-style: solid; border-width: 1px" height="26">Author: Phillip Ferland</td>
+	<td style="border-style: solid; border-width: 1px" height="26"><a class="links" href="http://ww.randomintervals.com/wifidb/ver.php#16b3">Version: 0.16 Build 3</a></td></tr>
+	<tr><td style="border-style: solid; border-width: 1px" height="26">Date: 2009-Jul-10</td></tr>
+	<tr><td style="border-style: solid; border-width: 1px" height="26">Archives:</td><td><a class="links" href="https://sourceforge.net/projects/vistumbler/files/WiFiDB/wifidb-alpha-016-b3.tar.gz/download"Tar Gz</a> <a class="links" href="https://sourceforge.net/projects/vistumbler/files/WiFiDB/wifidb-alpha-016-b3.zip/download"Zip</a></td></tr>
+	<tr><td style="border-style: solid; border-width: 1px" height="26">SVN:</td><td><a class="links" href="http://vistumbler.svn.sourceforge.net/viewvc/vistumbler/wifidb/DB/wifidb/?pathrev=413">SourceForge SVN</a></td></tr>
+</table>
+<br>
+<table width="100%" border="2" id="16pb1">
+	<tr><td style="border-style: solid; border-width: 1px" height="26">Author: Phillip Ferland</td>
 	<td style="border-style: solid; border-width: 1px" height="26"><a class="links" href="http://ww.randomintervals.com/wifidb/ver.php#16b3">Version: 0.16 Build 2.1</a></td></tr>
 	<tr><td style="border-style: solid; border-width: 1px" height="26">Date: 2009-May-05</td></tr>
 	<tr><td style="border-style: solid; border-width: 1px" height="26">Archives:</td><td><a class="links" href="https://sourceforge.net/project/showfiles.php?group_id=235720&package_id=286323&release_id=669476">SourceForge Download</a></td></tr>
