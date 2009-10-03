c73eb4f46b0b1461d7292f91b791032c1668038c
Andrew Calcutt <acalcutt@techidiots.net>
2009-10-03 03:08:25 +0000

    Fixed some website errors
    git-svn-id: https://svn.code.sf.net/p/vistumbler/code@485 ecfccc70-5fee-4fae-9362-317d472445a1

--- Website/Vistumbler.net/Downloads/_vti_cnf/index.php
+++ Website/Vistumbler.net/Downloads/_vti_cnf/index.php
@@ -1,14 +1,14 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|14 Jun 2009 22:12:20 -0000
-vti_extenderversion:SR|12.0.0.6211
+vti_timelastmodified:TR|30 Aug 2009 17:46:59 -0000
+vti_extenderversion:SR|12.0.0.4518
 vti_author:SR|Andrew-PC\\Andrew
-vti_modifiedby:SR|Andrew-PC\\Andrew
+vti_modifiedby:SR|WSC_DOMAIN\\acalcutt
 vti_timecreated:TR|14 Jun 2009 22:12:20 -0000
-vti_cacheddtm:TX|14 Jun 2009 22:12:20 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|30 Aug 2009 17:46:59 -0000
 vti_filesize:IR|68
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
 vti_cachedhastheme:BR|false
 vti_cachedhasborder:BR|false
 vti_charset:SR|utf-8
-vti_backlinkinfo:VX|
