2bc352f41f64a1925dd307a1a18de56f7f7aba88
Andrew Calcutt <acalcutt@techidiots.net>
2010-07-12 03:01:49 +0000

    Vistumbler v10 website link changes
    git-svn-id: https://svn.code.sf.net/p/vistumbler/code@633 ecfccc70-5fee-4fae-9362-317d472445a1

--- Website/Vistumbler.net/Downloads/_vti_cnf/index.php
+++ Website/Vistumbler.net/Downloads/_vti_cnf/index.php
@@ -1,11 +1,11 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|30 Aug 2009 17:46:59 -0000
+vti_timelastmodified:TR|24 Mar 2010 14:56:59 -0000
 vti_extenderversion:SR|12.0.0.4518
 vti_author:SR|Andrew-PC\\Andrew
 vti_modifiedby:SR|WSC_DOMAIN\\acalcutt
 vti_timecreated:TR|14 Jun 2009 22:12:20 -0000
 vti_backlinkinfo:VX|
-vti_cacheddtm:TX|30 Aug 2009 17:46:59 -0000
+vti_cacheddtm:TX|24 Mar 2010 14:56:59 -0000
 vti_filesize:IR|68
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
