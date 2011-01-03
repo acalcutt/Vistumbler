e2129ffd032a9904c58182f6b5214edbf303b66d
Phillip Ferland <pferland@randomintervals.com>
2011-01-03 20:34:25 +0000

    git-svn-id: https://svn.code.sf.net/p/vistumbler/code@691 ecfccc70-5fee-4fae-9362-317d472445a1

--- wifidb/tools/count_all_rows.php
+++ wifidb/tools/count_all_rows.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL|E_STRICT);
 global $screen_output;
 $screen_output = "CLI";
 
-if(!(require_once 'config.inc.php')){die("You need to create and configure your config.inc.php file in the [tools dir]/daemon/config.inc.php");}
+if(!(require_once 'daemon/config.inc.php')){die("You need to create and configure your config.inc.php file in the [tools dir]/daemon/config.inc.php");}
 if($GLOBALS['wifidb_install'] == ""){die("You need to edit your daemon config file first in: [tools dir]/daemon/config.inc.php");}
 require_once $GLOBALS['wifidb_install']."/lib/database.inc.php";
 require_once $GLOBALS['wifidb_install']."/lib/config.inc.php";
