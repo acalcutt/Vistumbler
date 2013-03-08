9b4e523de98f4fb162ffe8d787cd87944744e9d6
Andrew Calcutt <ACalcutt@techidiots.net>
2013-03-07 20:39:30 -0500

    Removed non Vistumbler files

--- wifidb/tools/md5.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo md5('pass');
-?>
--- wifidb/wifidb/install/patches.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../lib/database.inc.php');
-pageheader("Patches Page");
-echo '<title>Wireless DataBase *Alpha*'.$ver["wifidb"].' --> </title>';
-echo '<h2>Patching scripts for WiFiDB<h4><b>----------------------------------------------</b>';
-echo '<h4>If you have GPS points that are blank, or If your GPS dates are in the format [MM-DD-YYYY], Alter them to [YYYY-MM-DD], go <a class="links" href="patch_blank_gps/">here</a></h4><b>----------------------------------------------</b>';
-echo '<h4>To prevent GPS points from having null data in the fields named `hdp` , `alt` , `geo` , kmh` , and `track`. Need to update `lu`, and `fa` to support miliseconds, go <a class="links" href="patch_gps_table/patch_gps_tbl.php">here</a><BR>This is a quick and dirty script that goes through every %_GPS table and alters the columns from the wrong type (float() to varchar(255), float was having issues with some of the values)<h4>';
-footer($_SERVER['SCRIPT_FILENAME'])
-?>
