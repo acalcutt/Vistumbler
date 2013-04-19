e99b07db7d1c209a4a25b05c4b25827324841b9c
Phillip Ferland <pferland@randomintervals.com>
2013-04-19 18:43:16 -0400

    semi-working GPS
    GPS class will read from the network only, haven't figured out how to
    get it to show the settings alert if GPS is disabled.

--- WifiDB_Uploader/WifiDB_Uploader/src/com/eiri/wifidb_uploader/WiFiDemo.java
+++ WifiDB_Uploader/WifiDB_Uploader/src/com/eiri/wifidb_uploader/WiFiDemo.java
@@ -1,30 +1,24 @@
 package com.eiri.wifidb_uploader;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.IntentFilter;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class WiFiDemo extends Activity implements OnClickListener {
-	private static final String TAG = "WiFiDemo";
-	WifiManager wifi;
-	BroadcastReceiver receiver;
-
+	private static final String TAG = "WiFiDB_Demo";
+	Switch ScanSwitch;
 	TextView textStatus;
 	Button buttonScan;
+	MyResultReceiver resultReceiver;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -39,45 +33,44 @@ public class WiFiDemo extends Activity implements OnClickListener {
 	       .build());
 
 		// Setup UI
-		textStatus = (TextView) findViewById(R.id.textStatus);
-		buttonScan = (Button) findViewById(R.id.buttonScan);
-		buttonScan.setOnClickListener(this);
+		resultReceiver = new MyResultReceiver(null);
+		ScanSwitch = (Switch) findViewById(R.id.ScanSwitch);
+		ScanSwitch.setOnClickListener(this);
 
-		// Setup WiFi
-		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-		// Get WiFi status
-		WifiInfo info = wifi.getConnectionInfo();
-		textStatus.append("\n\nWiFi Status: " + info.toString());
-
-		// List available networks
-		List<WifiConfiguration> configs = wifi.getConfiguredNetworks();
-		for (WifiConfiguration config : configs) {
-			textStatus.append("\n\n" + config.toString());
-		}
-
-		// Register Broadcast Receiver
-		if (receiver == null)
-			receiver = new WiFiScanReceiver(this);
-
-		registerReceiver(receiver, new IntentFilter(
-				WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 		Log.d(TAG, "onCreate()");
 	}
 
-	@Override
-	public void onStop() {
-		unregisterReceiver(receiver);
+	public void onClick(View src) {
+		switch (src.getId()) {
+		    case R.id.ScanSwitch:
+		    	Log.d(TAG, "ScanSwitch Pressed");
+		      	ScanSwitch = (Switch) findViewById(R.id.ScanSwitch);
+		      	if (ScanSwitch.isChecked()){
+		      		Log.d(TAG, "Start Scan");
+		      		startService(new Intent(this, ScanService.class));
+		      		ScanSwitch.setChecked(true);
+		      	} else {
+		      		Log.d(TAG, "Stop Scan");
+		      		stopService(new Intent(this, ScanService.class));
+		      		ScanSwitch.setChecked(false);
+		        }
+		      	break;
+	    }
+
 	}
-
-	public void onClick(View view) {
-		Toast.makeText(this, "On Click Clicked. Toast to that!!!",
-				Toast.LENGTH_LONG).show();
-
-		if (view.getId() == R.id.buttonScan) {
-			Log.d(TAG, "onClick() wifi.startScan()");
-			wifi.startScan();
+	class MyResultReceiver extends ResultReceiver
+	{
+		public MyResultReceiver(Handler handler) {
+			super(handler);
 		}
+		
+		@Override
+		protected void onReceiveResult(int resultCode, Bundle resultData) 
+		{
+			Log.d(TAG, resultData.toString());
+			if(resultCode == 100){
+				textStatus.append("\n\nWiFi Status: " + resultData.toString());
+			}
+		}	
 	}
-
 }
--- WifiDB_Uploader/WifiDB_Uploader/src/com/eiri/wifidb_uploader/WiFiScanReceiver.java
+++ WifiDB_Uploader/WifiDB_Uploader/src/com/eiri/wifidb_uploader/WiFiScanReceiver.java
@@ -6,37 +6,39 @@ import java.util.List;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.net.wifi.ScanResult;
 import android.util.Log;
 
 
 public class WiFiScanReceiver extends BroadcastReceiver {
-  private static final String TAG = "WiFiScanReceiver";
-  WiFiDemo wifiDemo;
+  private static final String TAG = "WiFiDB_WiFiScanReceiver";
+  ScanService ScanService;
 
-  public WiFiScanReceiver(WiFiDemo wifiDemo) {
+  public WiFiScanReceiver(ScanService ScanService) {
     super();
-    this.wifiDemo = wifiDemo;
+    this.ScanService = ScanService;
   }
 
   @Override
   public void onReceive(Context c, Intent intent) {
 	  
-    List<ScanResult> results = wifiDemo.wifi.getScanResults();
+    List<ScanResult> results = ScanService.wifi.getScanResults();
 
     for (ScanResult result : results) {
-    	
-    	String bssid = result.BSSID;
-    	String ssid = result.SSID;
-    	int freq = result.frequency;
-    	String capabilities = result.capabilities;
-    	int level = result.level;
     	Log.d(TAG, "onReceive() message:1");
-    	GPS gps = new GPS();
-    	double[] loc = gps.getGPS();
     	WifiDB post = new WifiDB();
-    	post.postLiveData(ssid, bssid, capabilities, freq, level, loc[0], loc[1]);
-    	
+    	GPS gps = new GPS(c);
+    	if(!gps.canGetLocation())
+    	{
+    		gps.showSettingsAlert();
+    	}
+	    Location location = gps.getLocation();
+	    
+	    double latitude_str = location.getLatitude();
+	    double longitude_str = location.getLongitude();
+	    Log.d(TAG, "LAT: " + latitude_str + "LONG: " + longitude_str);
+    	post.postLiveData(result.SSID, result.BSSID, result.capabilities, result.frequency, result.level, latitude_str, longitude_str);
     }
 
   }
