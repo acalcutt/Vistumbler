587d4cee7cfb97ef2b9e544ece1b74a0e5f169d0
acalcutt <acalcutt@techidiots.net>
2019-06-29 11:30:45 -0400

    updated website template to match new wifidb layout
    with responsive design

--- Website/Vistumbler.net/js/_vti_cnf/linkanalytics.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|16 Jun 2015 23:58:04 -0000
-vti_extenderversion:SR|12.0.0.6211
-vti_backlinkinfo:VX|downloads.html verhist.html donate.htm Vistumbler.dwt index.html
